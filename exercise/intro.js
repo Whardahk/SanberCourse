2f3c70b4d8a077321c2cd2d0c57d1624f38f6e2d
Whardah <whardah25@gmail.com>
2022-11-17 21:15:46 +0700

    intro variable

--- exercise/intro.js
+++ exercise/intro.js
@@ -1,1 +1,13 @@
 console.log("Hello World !!!");
+
+var hello = "Hallo world";
+console.log(hello);
+
+var a = 5;
+a++;
+var b = 7;
+var c = a + b;
+console.log(c);
+
+var x = "10" + "add";
+console.log(x);
