49edc666df34a870205049ac1e66150019c94d4a
Whardah <whardah25@gmail.com>
2022-11-18 10:21:48 +0700

    condition can't input odd number

--- assignment/tugaspekan3.js
+++ assignment/tugaspekan3.js
@@ -5,4 +5,6 @@ var angka = prompt("Input angka: ");
 
 if (angka < 0) {
   console.log("Tidak bisa input bilangan negatif");
+} else if (angka % 2 == 1) {
+  console.log("Tidak bisa input bilangan ganjil");
 }
