70c6480ba7f77049d4079beb327be3d35d647f21
Whardah <whardah25@gmail.com>
2022-11-18 10:21:01 +0700

    condition can't input negative number

--- assignment/tugaspekan3.js
+++ assignment/tugaspekan3.js
@@ -2,3 +2,7 @@ console.log("No 1");
 const prompt = require("prompt-sync")({ sigint: true });
 
 var angka = prompt("Input angka: ");
+
+if (angka < 0) {
+  console.log("Tidak bisa input bilangan negatif");
+}
